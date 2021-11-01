4b36757b0366eecbff1e29db2401f3662bbb9da5
Md Abed Rahman <35268893+mdabedr@users.noreply.github.com>
2021-11-01 04:03:09 -0600

    Add files via upload

---
+++ LOF-LSCP-HDBSCAN.py
@@ -0,0 +1,235 @@
+"""Author Md Abed Rahman: mdabed@cs.ualberta.ca"""
+"""This method iteratively prunes outliers using
+both LOF and LSCP based outlier methods improve HDBSCAN’s performance"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+import matplotlib.gridspec as gridspec
+import timeit
+import gc
+from hdbscan import HDBSCAN
+# import DBCV as DBCV
+import seaborn as sns
+from scipy.stats import norm
+from scipy.stats import zscore
+from pyod.models.lscp import LSCP
+from pyod.models.lof import LOF
+from pyod.utils.utility import standardizer
+from pyod.utils.data import generate_data
+from pyod.utils.data import evaluate_print
+from pyod.utils.example import visualize
+
+gc.collect()
+print(__doc__)
+
+start = timeit.default_timer()
+
+np.random.seed(42)
+
+path = 'G:\\Poralekha\\UofA\Fall 2019\\CMPUT 697- Into to KDD and DM\\Project\\New datasets'
+
+np.random.seed(42)
+filepath = path + '\\DS6\\Tagged.txt'
+inputFile = pd.read_csv(filepath, sep='\t')
+
+# Read dataset
+df = pd.DataFrame([inputFile['X1'], inputFile['X2']])
+X = df.transpose()
+df = X
+X = np.asarray(X, dtype=np.float32)
+labels_true = inputFile['Cluster Number']
+
+outliers_pred=[]
+true_pred=[]
+
+extra_X=[]
+print(type(labels_true))
+para = len(X) * .1
+print("10% of the length of X is:", para)
+if para < 50.0:
+    detector_list = [LOF(n_neighbors=30), LOF(n_neighbors=40),
+                     LOF(n_neighbors=50), LOF(n_neighbors=60), LOF(n_neighbors=70)]
+else:
+    detector_list = [LOF(n_neighbors=60), LOF(n_neighbors=70),
+                     LOF(n_neighbors=80), LOF(n_neighbors=90), LOF(n_neighbors=100)]
+
+# Make plot grid of 2X2
+# gs = gridspec.GridSpec(2, 2)
+#
+# plt.figure()
+#
+# ax = plt.subplot(gs[0, 0])  # row 0, col 0
+
+counter = 0
+while (True):
+
+    # fit the model for outlier detection with LOF
+    clf = LocalOutlierFactor(n_neighbors=20)
+    y_pred = clf.fit_predict(X)
+    X_scores = clf.negative_outlier_factor_  # Default model has negative of LOF scores
+    # Get the true LOF scores
+    LOF_score = -X_scores
+
+    if counter == 0:
+        # Plot with LOF
+        # plt.title("Local Outlier Factor (LOF)")
+        # # plt.subplot(211)
+        # plt.xlim((df['X1'].min() - 100, df['X1'].max() + 100))
+        # plt.ylim((df['X2'].min() - 100, df['X2'].max() + 100))
+        #
+        # plt.scatter(X[:, 0], X[:, 1], color='k', s=3., label='Data points')
+        # # plot circles with radius proportional to the outlier scores
+        # radius = (X_scores.min() - X_scores) / (X_scores.min() - X_scores.max())
+        # plt.scatter(X[:, 0], X[:, 1], s=1000 * radius, edgecolors='r',
+        #             facecolors='none', label='Outlier scores')
+        # plt.axis('tight')
+        #
+        # # plt.xlabel("prediction errors: %d" % (n_errors))
+        # legend = plt.legend(loc='upper left')
+        # legend.legendHandles[0]._sizes = [10]
+        # legend.legendHandles[1]._sizes = [20]
+
+        old_max = X_scores.max()
+        old_min = X_scores.min()
+
+    # The usual way. Make a function for ease of use
+    length = LOF_score.size
+    flag = []
+    for i in range(0, length):
+        if LOF_score[i] > 2:
+            # print i
+            flag.append(i)
+
+    # Find the outlier percentage
+    outlier_percentage = float(len(flag)) / float(len(df))
+    print("Outlier percentage is", outlier_percentage*100)
+    if outlier_percentage <= 0.01:
+        print("breaking here")
+        break
+
+    # Now get rid of the global outliers
+    clf_name = 'LSCP'
+    # detector_list = [LOF(n_neighbors=30), LOF(n_neighbors=40),
+    #                  LOF(n_neighbors=50), LOF(n_neighbors=60),LOF(n_neighbors=70), LOF(n_neighbors=80),
+    #                  LOF(n_neighbors=90), LOF(n_neighbors=100)]
+    clf = LSCP(detector_list, contamination=outlier_percentage, random_state=42)
+    clf.fit(X)
+    y_train_pred = clf.labels_  # binary labels (0: inliers, 1: outliers)
+    y_train_scores = clf.decision_scores_  # raw outlier scores
+    length = len(y_train_pred)
+    flag = []
+    for i in range(0, length):
+        if y_train_pred[i] == 1:
+            # print i
+            flag.append(i)
+
+    # Check how many inliers are going to get pruned
+    count = 0
+    for i in flag:
+        if (labels_true[i] != -1):
+            count = count + 1
+
+    print('no of inliers pruned is ' + str(count))
+    # pruning happens
+    df = df.drop(df.index[flag])
+    LOF_score = np.delete(LOF_score, flag, 0)
+    X_scores = np.delete(X_scores, flag, 0)
+
+    extra_X.extend([X[i] for i in flag])
+    X = np.delete(X, flag, 0)
+    print(len(labels_true), len(flag))
+    outliers_pred.extend([-1] * len(flag))
+    true_pred.extend([labels_true[i] for i in flag])
+
+    if counter == 0:
+        labels_true = np.delete(labels_true.values, flag, 0)
+        counter = counter + 1
+    else:
+        labels_true = np.delete(labels_true, flag, 0)
+
+# print(np.where())
+#
+# ax = plt.subplot(gs[0, 1])  # row 0, col 1
+# plt.title("After getting rid of outliers by recursively using Local Outlier Factor (LOF)")
+# plt.xlim((df['X1'].min() - 100, df['X1'].max() + 100))
+# plt.ylim((df['X2'].min() - 100, df['X2'].max() + 100))
+# plt.scatter(X[:, 0], X[:, 1], color='k', s=3., label='Data points')
+# # plot circles with radius proportional to the outlier scores
+# # radius = (X_scores.min() - X_scores) / (X_scores.min() - X_scores.max())
+#
+# radius = (old_min - X_scores) / (old_min - old_max)
+# plt.scatter(X[:, 0], X[:, 1], s=1000 * radius, edgecolors='r',
+#             facecolors='none', label='Outlier scores')
+#
+# plt.axis('tight')
+#
+# legend = plt.legend(loc='upper left')
+# legend.legendHandles[0]._sizes = [10]
+# legend.legendHandles[1]._sizes = [20]
+# HDBSCAN Code
+X = StandardScaler().fit_transform(X)
+hdb = HDBSCAN(min_cluster_size=10, min_samples=5).fit(X)
+hdb_labels = hdb.labels_
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_hdb_ = len(set(hdb_labels)) - (1 if -1 in hdb_labels else 0)
+
+print('\n\n++ HDBSCAN Results')
+print('Estimated number of clusters: %d' % n_clusters_hdb_)
+# print('Elapsed time to cluster: %.4f s' % hdb_elapsed_time)
+# print('Homogeneity: %0.3f' % metrics.homogeneity_score(labels_true, hdb_labels))
+# print('Completeness: %0.3f' % metrics.completeness_score(labels_true, hdb_labels))
+# print('V-measure: %0.3f' % metrics.v_measure_score(labels_true, hdb_labels))
+print('Adjusted Rand Index: %0.3f' % metrics.adjusted_rand_score(labels_true, hdb_labels))
+print('Mislabeled points: %0.3f'% (np.sum([np.where(labels_true[i]!=-1,1,0) for i in np.where(hdb_labels==-1)])+np.sum([np.where(hdb_labels[i]!=-1,1,0) for i in np.where(labels_true==-1)])))
+
+# # print('Adjusted Mutual Information: %0.3f' % metrics.adjusted_mutual_info_score(labels_true, hdb_labels))
+# print('Silhouette Coefficient: %0.3f' % metrics.silhouette_score(X, hdb_labels))
+# print('Mislabeled outliers: %0.3f'% np.sum([np.where(hdb_labels[i]!=-1,1,0) for i in np.where(labels_true==-1)]))
+# print('DBCV: %0.3f' % DBCV.DBCV(X, hdb_labels))
+##############################################################################
+# Plot result
+import matplotlib.pyplot as plt
+
+# ax = plt.subplot(gs[1, :])
+# Black removed and is used for noise instead.
+hdb_unique_labels = set(hdb_labels)
+# db_unique_labels = set(db_labels)
+hdb_colors = plt.cm.Spectral(np.linspace(0, 1, len(
+    hdb_unique_labels)))  # db_colors = plt.cm.Spectral(np.linspace(0, 1, len(db_unique_labels)))# fig = plt.figure(figsize=plt.figaspect(0.5))# hdb_axis = fig.add_subplot('121')# db_axis = fig.add_subplot('122')
+for k, col in zip(hdb_unique_labels, hdb_colors):
+    if k == -1:
+        # Black used for noise.
+        col = 'k'
+
+    plt.plot(X[hdb_labels == k, 0], X[hdb_labels == k, 1], 'o', markerfacecolor=col, markeredgecolor='k',
+             markersize=6)  # for k, col in zip(db_unique_labels, db_colors):#     if k == -1:#         # Black used for noise.#         col = 'k'#
+#     db_axis.lot(X[db_labels == k, 0], X[db_labels == k, 1], 'o', markerfacecolor=col, #markeredgecolor='k', markersize=6)
+
+plt.title('LOF-LSCP-HDBSCAN\nEstimated number of clusters: %d' % n_clusters_hdb_)
+# db_axis.set_title('DBSCAN\nEstimated number of clusters: %d' % n_clusters_db_)
+plt.show()
+#True ARI
+labels_true=labels_true.tolist()
+labels_true.extend(true_pred)
+hdb_labels=hdb_labels.tolist()
+hdb_labels.extend(outliers_pred)
+X=X.tolist()
+X.extend(extra_X)
+print('Adjusted Rand Index: %0.3f' % metrics.adjusted_rand_score(labels_true, hdb_labels))
+
+print('Mislabeled points: %0.3f'% (np.sum([np.where(np.asarray(labels_true)[i]!=-1,1,0) for i in np.where(np.asarray(hdb_labels)==-1)])+np.sum([np.where(np.asarray(hdb_labels)[i]!=-1,1,0) for i in np.where(np.asarray(labels_true)==-1)])))
+# filepath=filepath.replace('Tagged.txt','LOF-LSCP-HDBSCANPoints.txt')
+#
+# with open(filepath, 'w') as file:
+#     file.writelines('\t'.join(str(j) for j in i) + '\n' for i in X)
+#
+# filepath=filepath.replace('Points.txt','cluster.txt')
+# hdb_labels = [x+1 for x in hdb_labels]
+# with open(filepath, 'w') as f:
+#     for _list in hdb_labels:
+#             #f.seek(0)
+#         f.write(str(_list)+'\n')
